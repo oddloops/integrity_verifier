37b9eeb4bb44bd6305afec2ccdb9010381a3e247
oddloops <vinciwu.vwu@gmail.com>
2026-01-25 19:56:55 -0500

    Created user input and input error checking

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,41 @@
+#include <cctype>
 #include <iostream>
+#include <limits>
 using namespace std;
 
-int main () {
-  string cliInput = "";
+#define RECORD_MODE 1
+#define VERIFY_MODE 2
+#define ATTEMPT_LIMITS 3
 
-  cout << "Type something or CTRL+C to exit: ";
-  cin >> cliInput;
-  cout << cliInput << endl;
+int main () {
+  int attempts = 0;
+  int modeSelected = -1;
+
+  cout << "Please selected a mode:" << endl;
+  cout << "(1) Record mode" << endl;
+  cout << "(2) Verify mode" << endl;
+  cout << "(Exit) Enter any key" << endl;
+  cout << "Mode: ";
+  cin >> modeSelected;
+
+  while (!cin && attempts < ATTEMPT_LIMITS) {
+    attempts++;
+    cout << "Bad input." << endl;
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Select mode: ";
+    cin >> modeSelected;
+  }
+
+  if (modeSelected == RECORD_MODE) {
+    cout << "Entering Record mode..." << endl;
+  }
+  else if (modeSelected == VERIFY_MODE) {
+    cout << "Entering Verification mode..." << endl;
+  }
+  else {
+    cout << "Exiting..."<< endl;
+  }
   
   return 0;
 }
