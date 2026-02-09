77979f540a866e9e3abd3d90a95e0c267ea1f6c2
oddloops <vinciwu.vwu@gmail.com>
2026-02-08 23:47:35 -0500

    Added test for validate message and exception throwing

--- tests/IntegrityCoreTest.cc
+++ tests/IntegrityCoreTest.cc
@@ -16,9 +16,43 @@ TEST_F(IntegrityCoreTestClass, InvalidDirectoryBool) {
   std::filesystem::remove_all("sandbox");
 }
 
+TEST_F(IntegrityCoreTestClass, InvalidDirectoryMessage) {
+  const std::filesystem::path p1 = "sandbox/t1";
+  std::filesystem::create_directories(p1);
+  const std::filesystem::path p2 = "sandbox/t2";
+  const std::string invalidMessage = "invalid directory path";
+  EXPECT_EQ(core.validateDirectory(p2).message, invalidMessage);
+  std::filesystem::remove_all("sandbox");
+}
+
 TEST_F(IntegrityCoreTestClass, ValidDirectoryBool) {
   const std::filesystem::path p = "sandbox/t1";
   std::filesystem::create_directories(p);
   EXPECT_TRUE(core.validateDirectory(p).validateBool);
+  std::filesystem::remove_all("sandbox");
+}
+
+TEST_F(IntegrityCoreTestClass, ValidDirectoryMessage) {
+  const std::filesystem::path p = "sandbox/t1";
+  std::filesystem::create_directories(p);
+  std::string validMessage = "valid directory path";
+  EXPECT_EQ(core.validateDirectory(p).message, validMessage);
+  std::filesystem::remove_all("sandbox");
+}
+
+TEST_F(IntegrityCoreTestClass, ValidateThrow) {
+  const std::filesystem::path p = "sandbox/t1";
+  std::filesystem::create_directories(p);
+  std::filesystem::permissions(
+			       "sandbox",
+			       std::filesystem::perms::owner_exec | std::filesystem::perms::group_exec | std::filesystem::perms::others_exec,
+			       std::filesystem::perm_options::remove
+			      );
+  EXPECT_FALSE(core.validateDirectory(p).validateBool);
+  std::filesystem::permissions(
+			       "sandbox",
+			       std::filesystem::perms::owner_all,
+			       std::filesystem::perm_options::add
+			      ); 
   std::filesystem::remove_all("sandbox");
 }
