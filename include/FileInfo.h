60851f3912636903d7cc8cd496e69a28a1c3d461
oddloops <vinciwu.vwu@gmail.com>
2026-02-08 20:37:10 -0500

    Defined new struct for file data

---
+++ include/FileInfo.h
@@ -0,0 +1,17 @@
+#ifndef FILEINFO_H
+#define FILEINFO_H
+
+#include <filesystem>
+#include <chrono>
+#include <string>
+
+struct FileInfo {
+  std::string fileName;
+  std::filesystem::path filePath;
+  std::string fileType;
+  std::string fileExtension;
+  std::string fileHash;
+  std::chrono::time_point<std::chrono::system_clock> recordTimestamp;
+};
+
+#endif
