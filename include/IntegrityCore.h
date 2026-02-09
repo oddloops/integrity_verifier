f14f535d176218aee5e78ca66e5dde0898de89c4
oddloops <vinciwu.vwu@gmail.com>
2026-02-08 20:36:29 -0500

    Added new include for new fileinfo and validatemessage struct

--- include/IntegrityCore.h
+++ include/IntegrityCore.h
@@ -2,14 +2,21 @@
 #define INTEGRITY_CORE_H
 
 #include <filesystem>
-#include <stdexcept>
 #include <iostream>
+#include <stdexcept>
 #include <string>
+#include <map>
+
+#include "ValidateMessage.h"
+#include "FileInfo.h"
 
 class IntegrityCore
 {
  public:
-  bool validateDirectory(const std::filesystem::path& targetDirectory);
+  ValidateMessage validateDirectory(const std::filesystem::path& targetDirectory);
+  std::string computeHash(const std::filesystem::path& filePath);
+  std::map<std::filesystem::path, FileInfo> getDirectoryContents(const std::filesystem::path& targetDirectory) const;
+  bool readRecord(const std::string& recordFile) const;
 };
 
 #endif
