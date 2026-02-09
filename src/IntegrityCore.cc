895478cbd231caba06e703c6ca738fb2d4bef445
oddloops <vinciwu.vwu@gmail.com>
2026-02-08 23:47:18 -0500

    Updated validate message output

--- src/IntegrityCore.cc
+++ src/IntegrityCore.cc
@@ -5,10 +5,10 @@ ValidateMessage IntegrityCore::validateDirectory(const std::filesystem::path& ta
   try {
     std::filesystem::file_status tDirectory = std::filesystem::status(targetDirectory);
     if (!std::filesystem::is_directory(tDirectory)) {
-      return ValidateMessage{false, "Path not a directory"};
+      return ValidateMessage{false, "invalid directory path"};
     }
     
-    return ValidateMessage{true, "Path is a directory"};
+    return ValidateMessage{true, "valid directory path"};
   }
   catch (std::filesystem::filesystem_error const& e) {
     std::cout << "Filesystem error: " << e.what() << std::endl;
