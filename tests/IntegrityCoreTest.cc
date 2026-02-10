d57914ff0931219ff4ac9ddee0dfc5ff549a95c3
oddloops <vinciwu.vwu@gmail.com>
2026-02-10 00:24:00 -0500

    Added new test for getDirectoryContent methods

--- tests/IntegrityCoreTest.cc
+++ tests/IntegrityCoreTest.cc
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <stdexcept>
 #include "../include/IntegrityCore.h"
+#include "TestHelpers.h"
 
 class IntegrityCoreTestClass : public testing::Test
 {
@@ -8,35 +9,19 @@ protected:
   IntegrityCore core;
 };
 
+// validateDirectory Tests
 TEST_F(IntegrityCoreTestClass, InvalidDirectoryBool) {
   const std::filesystem::path p1 = "sandbox/t1";
   std::filesystem::create_directories(p1);
   const std::filesystem::path p2 = "sandbox/t2";
-  EXPECT_FALSE(core.validateDirectory(p2).validateBool);
-  std::filesystem::remove_all("sandbox");
-}
-
-TEST_F(IntegrityCoreTestClass, InvalidDirectoryMessage) {
-  const std::filesystem::path p1 = "sandbox/t1";
-  std::filesystem::create_directories(p1);
-  const std::filesystem::path p2 = "sandbox/t2";
-  const std::string invalidMessage = "invalid directory path";
-  EXPECT_EQ(core.validateDirectory(p2).message, invalidMessage);
+  EXPECT_FALSE(core.validateDirectory(p2));
   std::filesystem::remove_all("sandbox");
 }
 
 TEST_F(IntegrityCoreTestClass, ValidDirectoryBool) {
   const std::filesystem::path p = "sandbox/t1";
   std::filesystem::create_directories(p);
-  EXPECT_TRUE(core.validateDirectory(p).validateBool);
-  std::filesystem::remove_all("sandbox");
-}
-
-TEST_F(IntegrityCoreTestClass, ValidDirectoryMessage) {
-  const std::filesystem::path p = "sandbox/t1";
-  std::filesystem::create_directories(p);
-  std::string validMessage = "valid directory path";
-  EXPECT_EQ(core.validateDirectory(p).message, validMessage);
+  EXPECT_TRUE(core.validateDirectory(p));
   std::filesystem::remove_all("sandbox");
 }
 
@@ -48,11 +33,22 @@ TEST_F(IntegrityCoreTestClass, ValidateThrow) {
 			       std::filesystem::perms::owner_exec | std::filesystem::perms::group_exec | std::filesystem::perms::others_exec,
 			       std::filesystem::perm_options::remove
 			      );
-  EXPECT_FALSE(core.validateDirectory(p).validateBool);
+  EXPECT_FALSE(core.validateDirectory(p));
   std::filesystem::permissions(
 			       "sandbox",
 			       std::filesystem::perms::owner_all,
 			       std::filesystem::perm_options::add
 			      ); 
   std::filesystem::remove_all("sandbox");
+}	       
+
+// getDirectoryContents Tests
+TEST_F(IntegrityCoreTestClass, getEmptyDirectory) {
+  const std::filesystem::path p = "sandbox/t1";
+  std::filesystem::create_directories(p);
+  std::map<std::filesystem::path, FileInfo> actualMap = core.getDirectoryContents(p);
+  std::map<std::filesystem::path, FileInfo> expectedMap{};
+  EXPECT_TRUE(TestHelpers::getDirectoryTestHelper(actualMap, expectedMap));
+  std::filesystem::remove_all("sandbox");
 }
+
