68ce1686c214b99cd506e3c6da87c69d7ade258a
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-02-10 23:41:26 -0500

    Added skeleton test for getDirectoryContents if exists

--- tests/IntegrityCoreTest.cc
+++ tests/IntegrityCoreTest.cc
@@ -1,3 +1,5 @@
+#include <cstdio>
+#include <fstream>
 #include <gtest/gtest.h>
 #include <stdexcept>
 #include "../include/IntegrityCore.h"
@@ -52,3 +54,29 @@ TEST_F(IntegrityCoreTestClass, getEmptyDirectory) {
   std::filesystem::remove_all("sandbox");
 }
 
+TEST_F(IntegrityCoreTestClass, getDirectoryContentsOutput) {
+  auto createFile = [](const std::filesystem::path& p, std::string_view pContents){
+    std::ofstream newFile(p);
+    newFile << pContents;
+  };
+  
+  const std::filesystem::path root = "sandbox";
+  const std::filesystem::path p1 = root/"c1";
+  const std::filesystem::path p2 = root/"c2";
+  std::filesystem::create_directories(p1);
+  std::filesystem::create_directories(p2);
+
+  createFile(p1/"fileP1.txt", "Hello c1");
+  createFile(p1/"config.json", "{ \"key\": 1 }");
+  createFile(p1/"image.png", "png");
+  createFile(p1/"fileP2.txt", "Hello c2");
+  createFile(p2/"script.py", "print('Hello World!')");
+  createFile(p2/"archive.zip", "dummy zip");
+ 
+  std::map<std::filesystem::path, FileInfo> actualMap = core.getDirectoryContents(root);
+  std::map<std::filesystem::path, FileInfo> expectedMap{
+    {p1/"fileP1.txt", FileInfo{}}
+  };
+  EXPECT_TRUE(TestHelpers::getDirectoryTestHelper(actualMap, expectedMap));
+  std::filesystem::remove_all("sandbox");
+}
