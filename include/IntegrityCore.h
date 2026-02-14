acee6d4aebb71f17a51eb23c50c95a21110a4842
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-02-14 10:36:54 -0500

    Added new private methods for setting and getting fileInfo

--- include/IntegrityCore.h
+++ include/IntegrityCore.h
@@ -6,6 +6,7 @@
 #include <stdexcept>
 #include <string>
 #include <map>
+#include <sys/types.h>
 
 #include "ValidateMessage.h"
 #include "FileInfo.h"
@@ -15,7 +16,18 @@ class IntegrityCore
  public:
   bool validateDirectory(std::filesystem::path const& targetDirectory) const;
   std::map<std::filesystem::path, std::map<std::filesystem::path, FileInfo>> getDirectoryContents(std::filesystem::path const& targetDirectory) const;
+  std::string computeHash(std::filesystem::path const& filePath) const;
+  FileInfo createFileInfo(std::filesystem::path const& directory_entry) const;
   bool readRecord(std::string const& recordFile) const;
+
+private:
+  void setFileInfo(FileInfo& fi, std::filesystem::path const& p);
+  std::string getFileName(std::filesystem::path& p) const;
+  std::string getFileExtension(std::filesystem::path const& p) const;
+  u_int64_t getFileSize(std::filesystem::path const& p) const;
+  std::filesystem::perms getPermissions(std::filesystem::path const& p) const;
+  std::chrono::time_point<std::chrono::system_clock> getLastModifiedTime(std::filesystem::path const& p) const;
+  std::chrono::time_point<std::chrono::system_clock> setRecordTime(std::filesystem::path const& p) const;
 };
 
 #endif
