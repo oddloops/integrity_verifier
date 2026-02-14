18ef57d03030cddfa55d817eef318b341c0f8da7
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-02-14 00:52:14 -0500

    Created helper function for testing FileInfo struct

--- tests/TestHelpers.h
+++ tests/TestHelpers.h
@@ -5,29 +5,26 @@
 #include "../include/FileInfo.h"
 
 namespace TestHelpers {
-  
-bool getDirectoryTestHelper(
-			    const std::map<std::filesystem::path, FileInfo>& m1,
-			    const std::map<std::filesystem::path, FileInfo>& m2
-			    ) {
-  if (m1.size() != m2.size()) {
-    return false;
+  bool getDirectoryTestHelper(
+			      const std::map<std::filesystem::path, FileInfo>& m1,
+			      const std::map<std::filesystem::path, FileInfo>& m2
+			      ) {
+    if (m1.size() != m2.size()) {
+      return false;
+    }
+    
+    for (const auto& [path, m1Info] : m1) {
+      auto m2Path = m2.find(path);
+      if (m2Path == m2.end()) return false;
+      
+      const FileInfo m2Info = m2Path->second;
+      if (m1Info.fileName != m2Info.fileName) return false;
+      if (m1Info.filePath != m2Info.filePath) return false;
+      if (m1Info.fileExtension != m2Info.fileExtension) return false;
+      if (m1Info.fileHash != m2Info.fileHash) return false;
+    }
+    return true;
   }
-
-  for (const auto& [path, m1Info] : m1) {
-    auto m2Path = m2.find(path);
-    if (m2Path == m2.end()) return false;
-
-    const FileInfo m2Info = m2Path->second;
-    if (m1Info.fileName != m2Info.fileName) return false;
-    if (m1Info.filePath != m2Info.filePath) return false;
-    if (m1Info.fileType != m2Info.fileType) return false;
-    if (m1Info.fileExtension != m2Info.fileExtension) return false;
-    if (m1Info.fileHash != m2Info.fileHash) return false;
-  }
-  return true;
-}
- 
 }
 
 #endif
