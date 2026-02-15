b19a277193fb3b1f8a4326ba635166d0330b4def
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-02-15 18:13:23 -0500

    Created separate h and c files for TestHelpers

---
+++ tests/TestHelpers.cc
@@ -0,0 +1,16 @@
+#include "../include/FileInfo.h"
+#include "include/TestHelpers.h"
+
+namespace TestHelpers {
+  void createFile(std::filesystem::path const& p, std::string_view pContents) {
+    std::ofstream newFile(p);
+    newFile << pContents;
+  }
+
+  bool compareFileInfo (FileInfo const& f1, FileInfo const& f2) {
+    if (f1.fileName != f2.fileName) return false;
+    if (f1.filePath != f2.filePath) return false;
+    if (f1.fileExtension != f2.fileExtension) return false;
+    return true;
+  }
+}
---
+++ tests/include/TestHelpers.h
@@ -0,0 +1,13 @@
+#ifndef TESTHELPERS_H
+#define TESTHELPERS_H
+
+#include <fstream>
+#include <map>
+#include "../include/FileInfo.h"
+
+namespace TestHelpers {
+  void createFile(std::filesystem::path const& p, std::string_view pContents);
+  bool compareFileInfo (FileInfo const& f1, FileInfo const& f2);
+}
+
+#endif
