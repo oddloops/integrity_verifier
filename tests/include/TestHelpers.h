ef9ee0defcd869fce5a767e55f688f8ba51cd1e5
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-02-16 23:53:49 -0500

    Created helper functions to print FileInfo data members

--- tests/include/TestHelpers.h
+++ tests/include/TestHelpers.h
@@ -8,6 +8,8 @@
 namespace TestHelpers {
   void createFile(std::filesystem::path const& p, std::string_view pContents);
   bool compareFileInfo (FileInfo const& f1, FileInfo const& f2);
+  std::ostream& fiOut(std::ostream& os, const FileInfo& fi);
+  std::string permsToString(std::filesystem::perms p);
 }
 
 #endif
