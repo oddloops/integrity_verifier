de6eae192fa078db21342a58943b3430f718d34d
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-02-16 23:52:05 -0500

    Created helper functions to print FileInfo data members

--- tests/TestHelpers.cc
+++ tests/TestHelpers.cc
@@ -13,4 +13,34 @@ namespace TestHelpers {
     if (f1.fileExtension != f2.fileExtension) return false;
     return true;
   }
+
+  std::ostream& fiOut(std::ostream& os, const FileInfo& fi) {
+    os << "FileInfo:\n"
+       << " Name: " << fi.fileName << "\n"
+       << " Path: " << fi.filePath << "\n"
+       << " Extension: " << fi.fileExtension << "\n"
+       << " Size: " << fi.fileSize << "\n"
+       << " Permissions: " << permsToString(fi.permissions) << "\n"
+       << " Hash: " << fi.fileHash << "\n";
+      //       << " Last Modified: " << fi.lastModified << "\n"
+      //       << " Timestamp: " << fi.recordTimestamp << "\n";
+    return os;
+  }
+
+  std::string permsToString(std::filesystem::perms p) {
+    std::string s;
+    s += (p & std::filesystem::perms::owner_read)  != std::filesystem::perms::none ? 'r' : '-';
+    s += (p & std::filesystem::perms::owner_write) != std::filesystem::perms::none ? 'w' : '-';
+    s += (p & std::filesystem::perms::owner_exec)  != std::filesystem::perms::none ? 'x' : '-';
+
+    s += (p & std::filesystem::perms::group_read)  != std::filesystem::perms::none ? 'r' : '-';
+    s += (p & std::filesystem::perms::group_write) != std::filesystem::perms::none ? 'w' : '-';
+    s += (p & std::filesystem::perms::group_exec)  != std::filesystem::perms::none ? 'x' : '-';
+
+    s += (p & std::filesystem::perms::others_read)  != std::filesystem::perms::none ? 'r' : '-';
+    s += (p & std::filesystem::perms::others_write) != std::filesystem::perms::none ? 'w' : '-';
+    s += (p & std::filesystem::perms::others_exec)  != std::filesystem::perms::none ? 'x' : '-';
+
+    return s;
+  }
 }
