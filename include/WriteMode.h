83e5706ae4928b3edc5d6a151a9303022dcf347b
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-05-16 15:58:20 -0400

    Remove redundant public access specifier

--- include/WriteMode.h
+++ include/WriteMode.h
@@ -11,13 +11,11 @@ class WriteMode
 {
 public:
   explicit WriteMode (IntegrityCore& core) : _core(core){}
-
-  public:
   bool run(std::filesystem::path const& directoryPath,
 	   std::filesystem::path const& outputPath); // calls writeRecord
   std::string getErrorMessage() const { return _errorMessage; }
   
-  private:
+private:
   IntegrityCore& _core;
   std::string _errorMessage;
   
