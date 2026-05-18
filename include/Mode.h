9b65f34c976ff20a326eaab85de3e4936e8c0431
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-05-17 22:57:11 -0400

    Created abstract class Mode

---
+++ include/Mode.h
@@ -0,0 +1,32 @@
+#ifndef MODE_CLASS
+#define MODE_CLASS
+
+#include "IntegrityCore.h"
+#include "DirectoryContent.h"
+#include "ModeContext.h"
+
+#include <filesystem>
+#include <string>
+
+class Mode
+{
+public:
+  explicit Mode (IntegrityCore& core) : _core(core){}
+  virtual ~Mode() = default;
+
+  virtual bool run(ModeContext const& ctx)=0;
+
+  std::string const& getErrorMsg() const {
+    return _errorMessage;
+  }
+
+protected:
+  IntegrityCore _core;
+  std::string _errorMessage;
+
+  void setErrorMsg(std::string msg) {
+    _errorMessage = msg;
+  }
+};
+
+#endif
---
+++ include/ModeContext.h
@@ -0,0 +1,12 @@
+#ifndef MODE_CONTEXT
+#define MODE_CONTEXT
+
+#include <filesystem>
+
+struct ModeContext {
+  std::filesystem::path directoryPath;
+  std::filesystem::path outputPath;
+  std::filesystem::path snapshotPath;
+}
+
+#endif
