a4206d34c1e23484e93e5a1aaeea10cf9b1b5623
oddloops <36609544+vwwuu@users.noreply.github.com>
2026-06-20 23:19:59 -0400

    Add verify mode implementation

---
+++ include/modes/VerifyMode.h
@@ -0,0 +1,21 @@
+#ifndef VERIFYMODE_H
+#define VERIFYMODE_H
+
+#include "Mode.h"
+#include "WriteMode.h"
+
+class VerifyMode : public Mode
+{
+public:
+  using Mode::Mode;
+  bool run(ModeContext const& ctx) override;
+  
+private:
+  bool verifyRecord(std::filesystem::path const& recordPath,
+		    std::filesystem::path const& directoryPath);
+  bool verifyTraversal(DirectoryContent const& recordContent,
+		       DirectoryContent const& currenContent);
+  WriteMode _writer;
+};
+
+#endif
